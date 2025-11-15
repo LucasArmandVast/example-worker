9e92fa939a5198944b2a5d7697a1114786ee8ac8
Lucas Armand <lucas@vast.ai>
2025-11-14 16:12:43 -0800

    more sanity prints

--- worker.py
+++ worker.py
@@ -1,6 +1,7 @@
 from vastai import Worker
 from vastai.serverless.server.templates.vllm import vLLM
 
+print("configuring worker")
 worker_config = vLLM()
 print("starting worker")
 Worker(worker_config).run()
